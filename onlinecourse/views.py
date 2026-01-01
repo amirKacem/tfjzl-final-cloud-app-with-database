5940974b203fa1ca6ca5ee342b3b2152ae3f29bb
amirKacem <amir.kassem.ben.ammou@gmail.com>
2026-01-01 17:32:45 -0500

    show result

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -112,13 +112,13 @@ def enroll(request, course_id):
          # Redirect to show_exam_result with the submission id
 def submit(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
-    current_user = request.user
-    enrollement = Enrollment.objects.get(user=current_user)
+    user = request.user
+    enrollment = Enrollment.objects.get(user=user, course=course)
     submission = Submission.objects.create(enrollment=enrollment)
     choices = extract_answers(request)
     submission.choices.set(choices)
     submission_id = submission.id
-    return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result',args(course_id,submission_id,)))
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id,)))
 
 
 
@@ -142,19 +142,22 @@ def extract_answers(request):
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
 def show_exam_result(request, course_id, submission_id):
-
+    context = {}
     course = get_object_or_404(Course, pk=course_id)
     submission = Submission.objects.get(id=submission_id)
-    submission.choices.all()
+    choices = submission.choices.all()
 
     total_score = 0
-    questions = course.question_set.all() 
+    questions = course.question_set.all()  # Assuming course has related questions
+
     for question in questions:
-        correct_choices = question.choice_set.filter(is_correct=True)
-        selected_choices = choices.filter(question=question)
+        correct_choices = question.choice_set.filter(is_correct=True)  # Get all correct choices for the question
+        selected_choices = choices.filter(question=question)  # Get the user's selected choices for the question
+
+        # Check if the selected choices are the same as the correct choices
         if set(correct_choices) == set(selected_choices):
-            total_score += question.grade
-    
+            total_score += question.grade  # Add the question's grade only if all correct answers are selected
+
     context['course'] = course
     context['grade'] = total_score
     context['choices'] = choices
